836e5c00de72951bdd34564cbf5258d15490cf2e
elivandro <elivandro78@gmail.com>
2025-08-25 18:00:33 -0300

    criando component footer

--- src/components/footer/Footter.tsx
+++ src/components/footer/Footter.tsx
@@ -1,24 +1,165 @@
-import {ConteinerMain,ConteineTitulo,ConteinerCenter,ConteinerFooter,Itens} from "./footerCss"
-export const FootComponet = ()=>{
-    return(
-        <ConteinerMain>
-          <ConteineTitulo>
-            <h1>10+</h1>
-            <h1>10+</h1>
-            <h1>10+</h1>
-            <h1>10+</h1>
-          </ConteineTitulo>
-          <ConteinerCenter>
-            <h1>Baixe o App</h1>
-            <h5>Tenha o zapdai sem com voce. Peça com facilidade e acompanhe seu <br/>delivery em tempo real</h5>
-            <button>App Storage</button>
-          </ConteinerCenter>
-          <ConteinerFooter>
-            <Itens>
-                <h1>Zapdai</h1>
-                <button>Nosso Planos</button>
-            </Itens>
-          </ConteinerFooter>
-        </ConteinerMain>
-    )
+import {Titulo,SubTitulo2,FootBottom, Hr,ConteinerMain,Logo,SubMunu, ConteineTitulo,Storage,SubMunuItens, BtnDivide, ConteinerCenter, ConteinerFooter, Itens, IconeItens, H1, SubTitule } from "./footerCss"
+import logo from "../../assets/logowhite.png"
+const itens = [
+  {
+    h1: "10+", sub: "Empresas Parceiras", color: ""
+  },
+  {
+    h1: "5K+", sub: "Pedidos Entregues", color: ""
+  },
+  {
+    h1: "30min", sub: "Tempo Médio", color: ""
+  },
+  {
+    h1: "4.8★", sub: "Avaliação Média", color: ""
+  }
+]
+
+export const FootComponet = () => {
+
+  return (
+    <ConteinerMain>
+      <ConteineTitulo>
+        {itens.map(item => (
+          <IconeItens>
+            <H1>{item.h1}</H1>
+            <SubTitule>{item.sub}</SubTitule>
+          </IconeItens>
+        ))}
+      </ConteineTitulo>
+      <ConteinerCenter>
+        <h1>Baixe o App</h1>
+        <SubTitule>Tenha o zapdai sempre com você. Peça com facilidade e acompanhe seu delivery em tempo real.</SubTitule>
+        <BtnDivide>
+          <Storage color={false}>App Storage</Storage>
+          <Storage color={false}>App Storage</Storage>
+        </BtnDivide>
+      </ConteinerCenter>
+      <ConteinerFooter>
+        <Itens>
+          <Logo src={logo} />
+          <Storage color={true}>Nosso Planos</Storage>
+        </Itens>
+        <Hr></Hr>
+        <SubMunu>
+          {Menu.map(item=>(
+            <SubMunuItens>
+            <Titulo>{item.h1}</Titulo>
+            {
+              item.children.map(e=>(
+                <>
+                 <SubTitulo2>{e.titulo}</SubTitulo2>
+                </>
+              ))
+            }
+            
+          </SubMunuItens>
+          ))}
+        </SubMunu>
+         <FootBottom>
+         <Titulo>Redes Sociais</Titulo>
+          </FootBottom>
+        <Hr></Hr>
+      </ConteinerFooter>
+       <FootBottom>
+            © 2024 Zapdai. Todos os direitos reservados.
+        </FootBottom>
+    </ConteinerMain>
+  )
 }
+
+
+const Menu = [
+  {
+    h1: "Empresa", children:[
+      {
+        titulo:"Sobre nós",
+        link:""
+      },
+       {
+        titulo:"Carreiras",
+        link:""
+      },
+       {
+        titulo:"Blog",
+        link:""
+      },
+       {
+        titulo:"Imprensa",
+        link:""
+      }
+    ], color: ""
+  },
+  {
+    h1: "Parceiros", children:[
+      {
+        titulo:"Seja um restaurante",
+        link:""
+      },
+       {
+        titulo:"Seja um entregador",
+        link:""
+      },
+       {
+        titulo:"Zapdai Empresas",
+        link:""
+      }
+    ], color: ""
+  },
+  {
+    h1: "Serviços da Plataforma",children:[
+      {
+        titulo:"Cardápio Digital",
+        link:""
+      },
+       {
+        titulo:"Sistema de Pedidos",
+        link:""
+      },
+       {
+        titulo:"Serviços de Entregas",
+        link:""
+      },
+       {
+        titulo:"Atendente Virtual do WhatsApp",
+        link:""
+      },
+       {
+        titulo:"Ver todos Serviços Disponíveis",
+        link:""
+      }
+    ], color: ""
+  },
+  {
+    h1: "Suporte", children:[
+      {
+        titulo:"Central de Ajuda",
+        link:""
+      },
+       {
+        titulo:"Como Comprar no Zapdai",
+        link:""
+      },
+       {
+        titulo:"",
+        link:""
+      },
+       {
+        titulo:"Como Cadastrar seu Negócio",
+        link:""
+      },
+      {
+        titulo:"Contato",
+        link:""
+      },
+      {
+        titulo:"Termos de uso",
+        link:""
+      },
+      {
+        titulo:"Privacidade",
+        link:""
+      }
+    ], color: ""
+  }
+]
--- src/components/footer/footerCss.tsx
+++ src/components/footer/footerCss.tsx
@@ -6,6 +6,7 @@ flex-direction: column;
 background-color: #171718eb;
 color: #f5f5f5;
 height: 100vh;
+margin:0;
 `
 export const ConteineTitulo = styled.div`
 display: flex;
@@ -18,19 +19,123 @@ justify-content: space-between;
 export const ConteinerCenter = styled.div`
 display: flex;
 flex-direction: column;
-padding: 25px 0;
-margin: 10px;
+padding: 25px 46px;
 gap: 25px;
-align-self: center;
+align-items:center;
+justify-content: center;
+background:var(--background);
+@media screen and (min-width: 786px) {
+    padding: 25px 256px;
+
+}
 `
-export const ConteinerFooter= styled.div`
+export const ConteinerFooter = styled.div`
 display: flex;
 flex-direction: column;
-padding: 25px 250px;
+padding: 25px 256px;
 gap: 25px;
 height: 100%;
 `
 export const Itens = styled.div`
 display: flex;
 justify-content: space-between;
+padding:0 10px;
+
+`
+export const IconeItens = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const H1 = styled.h1`
+display: flex;
+flex-direction: column;
+color:orange;
+font-size: 38px;
+`
+export const SubTitule = styled.span`
+font-size:18px;
+font-weight:300;
+font-family:Arial, Helvetica, sans-serif;
+
+`
+
+export const BtnDivide = styled.div`
+   display: flex;
+   width: 100%;
+    justify-content:space-around;
+   padding:10px 0;
+   @media screen and (min-width: 700px) {
+       width: 80%;
+        padding:10px 56px;
+        justify-content:space-evenly;
+
+   }
+
+`
+type size = {
+    color:boolean
+}
+export const Storage = styled.button<size>`
+padding:15px 30px;
+border-radius:5px;
+border:1px solid #999;
+  background-color: ${({ color }) => (color ? '#f0742d' : '#141414')};
+
+color:#FFF;
+
+cursor: pointer;
+
+
+`
+
+export const Logo = styled.img`
+object-fit:contain;
+width: 100px;
+height: 50px;
+`
+export const Hr = styled.div`
+  height: 0.5px;
+  background-color: rgba(255, 255, 255, 0.1);
+  margin: 10px 0;
+`
+
+export const SubMunu = styled.div`
+ display: flex;
+ justify-content:space-between;
+ width: 100%;
+`
+
+export const SubMunuItens = styled.div`
+display: flex;
+flex-direction: column;
+padding: 10px;
+ 
+`
+
+export const Titulo = styled.span`
+color: orange;
+font-family:bold;
+font-family:Arial, Helvetica, sans-serif;
+font-size:18px;
+ 
+`
+export const SubTitulo2 = styled.small`
+font-family:Arial, Helvetica, sans-serif;
+font-size:15px;
+margin:10px 0;
+font-weight:400;
+cursor: pointer;
+color: #888;
+
+ 
+`
+export const FootBottom = styled.div`
+display: flex;
+flex-direction: column;
+padding: 10px;
+align-items:center;
+justify-content: center;
+color: #888;
+ 
 `
