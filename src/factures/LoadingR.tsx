ff60a3d49be22a9f0e75d1c6c687c07861a13ebd
elivandro <elivandro78@gmail.com>
2025-08-27 09:44:02 -0300

    adicionando docker file na pasta

--- src/factures/LoadingR.tsx
+++ src/factures/LoadingR.tsx
@@ -1,16 +1,18 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {LoadingRota, Section}  from "./loading"
 import { useNavigate } from "react-router-dom";
 export const LoadingR = ()=>{
-    const ativo = true;
+    const [ativo,setAtivo]  = useState(true);
     const nagivate = useNavigate();
 
     useEffect(()=>{
        const timeout =  setInterval(()=>{
          if(ativo){
              nagivate("/marketPlace")
+             setAtivo(false)
          }else{
              nagivate("/login")
+              setAtivo(false)
  
          }
         },500)
